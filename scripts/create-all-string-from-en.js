384d4233193493880bee969933c79cc143d1e52e
Aelto <Aelto@users.noreply.github.com>
2020-07-27 23:36:52 +0200

    update release directory with the current version

---
+++ scripts/create-all-string-from-en.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const path = require('path');
+
+const strings_directory = path.join(
+  __dirname,
+  'strings'
+);
+
+const FILE_TO_COPY = "en.w3strings";
+const full_path_to_file_to_copy = path.join(
+  strings_directory,
+  FILE_TO_COPY
+);
+
+const LANGUAGES = [
+  'ar',
+  'br',
+  'cz',
+  'de',
+  'es',
+  'esmx',
+  'fr',
+  'hu',
+  'it',
+  'jp',
+  'kr',
+  'pl',
+  'ru',
+  'zh'
+];
+
+const FILE_EXTENSION = 'w3strings';
+
+for (const language of LANGUAGES) {
+  fs.copyFileSync(
+    full_path_to_file_to_copy,
+    path.join(strings_directory, `${language}.${FILE_EXTENSION}`)
+  );
+}
+
+for (const file of fs.readdirSync(strings_directory)) {
+  if (file === FILE_TO_COPY) {
+    continue;
+  }
+
+  
+}
+
+console.log('done');
