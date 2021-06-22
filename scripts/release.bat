6055839cc1f614ad194cc01d4f9ba9ab1656a102
Aelto <Aelto@users.noreply.github.com>
2021-06-22 21:01:56 +0200

    update the script to include the sharedutils helpers package

--- scripts/release.bat
+++ scripts/release.bat
@@ -29,8 +29,9 @@ XCOPY "%modpath%\shared-utils\mod_sharedutils_mappins\" "%modpath%\release\mods\
 XCOPY "%modpath%\shared-utils\mod_sharedutils_dialogChoices\" "%modpath%\release\mods\mod_sharedutils_dialogChoices\" /e /s /y
 XCOPY "%modpath%\shared-utils\mod_sharedutils_npcInteraction\" "%modpath%\release\mods\mod_sharedutils_npcInteraction\" /e /s /y
 XCOPY "%modpath%\shared-utils\mod_sharedutils_noticeboards\" "%modpath%\release\mods\mod_sharedutils_noticeboards\" /e /s /y
+XCOPY "%modpath%\shared-utils\mod_sharedutils_helpers\" "%modpath%\release\mods\mod_sharedutils_helpers\" /e /s /y
 
-@REM it's an optional patch, so it is not includeed
+@REM it's an optional patch, so it is not included
 @REM XCOPY "%modpath%\shared-utils\mod0000_sharedutilsmappinsfhudpatch\" "%modpath%\release\mods\mod0000_sharedutilsmappinsfhudpatch\" /e /s /y
 
 if "%1"=="-github" (
