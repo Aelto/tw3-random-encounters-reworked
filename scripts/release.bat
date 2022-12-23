c192cb362eaa5140f91160de0f8b611eb1af4682
Aelto <Aelto@users.noreply.github.com>
2022-12-23 13:42:04 +0100

    copy dependencies to release folder

--- scripts/release.bat
+++ scripts/release.bat
@@ -34,6 +34,9 @@ XCOPY "%modpath%\shared-utils\mod_sharedutils_helpers\" "%modpath%\release\mods\
 XCOPY "%modpath%\shared-utils\mod_sharedutils_custombossbar\" "%modpath%\release\mods\mod_sharedutils_custombossbar\" /e /s /y
 XCOPY "%modpath%\shared-utils\mod_sharedutils_damagemodifiers\" "%modpath%\release\mods\mod_sharedutils_damagemodifiers\" /e /s /y
 
+REM Dependencies
+XCOPY "%modpath%\dependencies\" "%modpath%\release\" /e /s /y
+
 @REM it's an optional patch, so it is not included
 @REM XCOPY "%modpath%\shared-utils\mod0000_sharedutilsmappinsfhudpatch\" "%modpath%\release\mods\mod0000_sharedutilsmappinsfhudpatch\" /e /s /y
 
