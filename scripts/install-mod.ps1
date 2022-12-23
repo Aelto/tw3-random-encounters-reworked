62f62a95305650b04de2e1e164983e7c2e441158
Aelto <Aelto@users.noreply.github.com>
2022-12-23 12:55:05 +0100

    Update install-mod.ps1

--- scripts/install-mod.ps1
+++ scripts/install-mod.ps1
@@ -1,11 +1,17 @@
 # https://github.com/Aelto/tw3-random-encounters-reworked/releases/tag/dependencies-v1.0
+# https://github.com/Aelto/tw3-random-encounters-reworked/releases/latest/download/tw3-random-encounters-reworked.zip
 
 cls
 
 echo ""
 echo "Installer for the mod Random Encounters Reworked"
-echo "This will install Random Encounters and its dependencies based on the mods you have"
 echo ""
+echo "This will work in major steps:"
+echo " - First it will download the mods and its dependencies from GitHub"
+echo " - It will remove dependencies you may not need based on your current mods"
+echo " - It will list everything it plans to add to your game"
+echo " - It will install the listed files"
+echo " - It will open a link in your browser to guide you through the merging process and the first steps while in game"
 
 pause
 cls
@@ -27,111 +33,87 @@ if (!(test-path mods)) {
   mkdir mods | out-null
 }
 
-$dependenciesRelease = "https://api.github.com/repos/Aelto/tw3-random-encounters-reworked/releases/52800894"
-$dependenciesReleaseResponse = Invoke-RestMethod -Uri $dependenciesRelease
+
 
 $extractingFolder = "./__install_rer"
 $extractingFolderPatches = "./__install_rer_patches"
-$assetCommunityPatchBase = $dependenciesReleaseResponse.assets | ? {$_.name.StartsWith("community-patch-base")} | Select -First 1
-$assetSharedImport = $dependenciesReleaseResponse.assets | ? {$_.name.StartsWith("sharedimport")} |Select -First 1
-$assetBootstrap = $dependenciesReleaseResponse.assets | ? {$_.name.StartsWith("bootstrap")} |Select -First 1
-$assetEePatches = $dependenciesReleaseResponse.assets | ? {$_.name.StartsWith("ee-patches")} |Select -First 1
-$assetFhudPatches = $dependenciesReleaseResponse.assets | ? {$_.name.StartsWith("fhud-patches")} |Select -First 1
-
-# do not install community-patch-base if the user already has it or if has EE
-if (!(test-path mods/modW3EE) -and !(test-path mods/modW3EEMain)) {
-  echo ""
-  echo "downloading community-patch-base"
-
-  Invoke-WebRequest -Uri $assetCommunityPatchBase.browser_download_url -OutFile $assetCommunityPatchBase.name
-  Expand-Archive -Force -LiteralPath $assetCommunityPatchBase.name -DestinationPath ./$extractingFolder
-  remove-item $assetCommunityPatchBase.name -recurse -force
-}
-
-# do not install bootstrap if the user already has it
-if (!(test-path mods/modBootstrap-registry)) {
-  echo ""
-  echo "downloading bootstrap"
-
-
-  Invoke-WebRequest -Uri $assetBootstrap.browser_download_url -OutFile $assetBootstrap.name
-  Expand-Archive -Force -LiteralPath $assetBootstrap.name -DestinationPath ./$extractingFolder
-  remove-item $assetBootstrap.name -recurse -force
-}
-
-# do not install sharedimport if the user already has it or if he has EE
-if (!(test-path mods/modSharedImports) -and !(test-path mods/modW3EE) -and !(test-path mods/modW3EEMain)) {
-  echo ""
-  echo "downloading shared import"
-
-  Invoke-WebRequest -Uri $assetSharedImport.browser_download_url -OutFile $assetSharedImport.name
-  Expand-Archive -Force -LiteralPath $assetSharedImport.name -DestinationPath ./$extractingFolder
-  remove-item $assetSharedImport.name -recurse -force
-}
 
 echo ""
 echo "downloading Random Encounters Reworked"
 
 echo "fetching latest release"
 
-$allReleases = "https://api.github.com/repos/Aelto/tw3-random-encounters-reworked/releases"
-$allReleasesResponse = Invoke-RestMethod -Uri $allReleases
+$latestAssetUrl = "https://github.com/Aelto/tw3-random-encounters-reworked/releases/latest/download/tw3-random-encounters-reworked.zip"
+$latestAssetName = "tw3-random-encounters-reworked.zip"
+Invoke-WebRequest -Uri $latestAssetUrl -OutFile $latestAssetName
 
-$latestRelease = $allReleasesResponse | ? {!($_.name.StartsWith("dependencies"))} | Select -First 1
-$latestAsset = $latestRelease.assets[0]
+Expand-Archive -Force -LiteralPath $latestAssetName -DestinationPath ./$extractingFolder
+remove-item $latestAssetName -recurse -force
 
-write-host "downloading RER $($latestRelease.name)"
-Invoke-WebRequest -Uri $latestAsset.browser_download_url -OutFile $latestAsset.name
-
-Expand-Archive -Force -LiteralPath $latestAsset.name -DestinationPath ./$extractingFolder
-remove-item $latestAsset.name -recurse -force
-
+# TODO:
 # if the user has EE, install the patches for the improved compatibility
 # this is done after the RER download because it will overwrite files
-if (test-path mods/modW3EE) {
-  echo ""
-  echo "downloading W3EE compatibility patches"
+# if (test-path mods/modW3EE) {
+#   echo ""
+#   echo "downloading W3EE compatibility patches"
 
-  Invoke-WebRequest -Uri $assetEePatches.browser_download_url -OutFile $assetEePatches.name
-  Expand-Archive -Force -LiteralPath $assetEePatches.name -DestinationPath ./$extractingFolderPatches
-  remove-item $assetEePatches.name -recurse -force
-}
+#   Invoke-WebRequest -Uri $assetEePatches.browser_download_url -OutFile $assetEePatches.name
+#   Expand-Archive -Force -LiteralPath $assetEePatches.name -DestinationPath ./$extractingFolderPatches
+#   remove-item $assetEePatches.name -recurse -force
+# }
 
+# TODO:
 # if the user has FHUD, there is a patch for it to display custom 3D map markers
-if (!(test-path mods/modW3EE) -and (test-path mods/modFriendlyHUD)) {
-  echo ""
-  echo "downloading FHUD compatibility patches"
+# if (!(test-path mods/modW3EE) -and (test-path mods/modFriendlyHUD)) {
+#   echo ""
+#   echo "downloading FHUD compatibility patches"
 
-  Invoke-WebRequest -Uri $assetFhudPatches.browser_download_url -OutFile $assetFhudPatches.name
-  Expand-Archive -Force -LiteralPath $assetFhudPatches.name -DestinationPath ./$extractingFolderPatches
-  remove-item $assetFhudPatches.name -recurse -force
-}
+#   Invoke-WebRequest -Uri $assetFhudPatches.browser_download_url -OutFile $assetFhudPatches.name
+#   Expand-Archive -Force -LiteralPath $assetFhudPatches.name -DestinationPath ./$extractingFolderPatches
+#   remove-item $assetFhudPatches.name -recurse -force
+# }
 
 # print message notifying which mod will be installed
-$children = Get-ChildItem ./$extractingFolder
 cls
 echo ""
 echo "installing the following mods:"
+$children = Get-ChildItem ./$extractingFolder/mods
 foreach ($child in $children) {
   write-host " - $($child.name)"
 }
 
-if (test-path $extractingFolderPatches) {
-  echo ""
-  echo "installing the following patches:"
-  $childrenPatches = Get-ChildItem ./$extractingFolderPatches
-
-  foreach ($child in $childrenPatches) {
-    write-host " - $($child.name)"
-  }
+echo ""
+echo "installing the following dlcs:"
+$children = Get-ChildItem ./$extractingFolder/dlc
+foreach ($child in $children) {
+  write-host " - $($child.name)"
 }
+
+echo ""
+echo "installing the following menus:"
+$children = Get-ChildItem ./$extractingFolder/bin/config/r4game/user_config_matrix/pc
+foreach ($child in $children) {
+  write-host " - $($child.name)"
+}
+
+# TODO:
+# if (test-path $extractingFolderPatches) {
+#   echo ""
+#   echo "installing the following patches:"
+#   $childrenPatches = Get-ChildItem ./$extractingFolderPatches
+
+#   foreach ($child in $childrenPatches) {
+#     write-host " - $($child.name)"
+#   }
+# }
 
 echo ""
 pause
 
 # finally start installing the mods
+$children = Get-ChildItem ./$extractingFolder
 foreach ($child in $children) {
-  $fullpath = "{0}/{1}/*" -f $extractingFolder, $child
+  $fullpath = "{0}/{1}" -f $extractingFolder, $child
   copy-item $fullpath . -force -recurse
 
   write-host "$($child) installed"
@@ -155,94 +137,4 @@ if (test-path $extractingFolderPatches) {
   remove-item $extractingFolderPatches -recurse -force
 }
 
-
-# and run the update registry script
-echo ""
-echo "Updating bootstrap registry for Random Encounters Reworked"
-cd ./mods/modRandomEncountersReworked/
-./update-registry.bat
-cd ../../
-
-# final message
-cls
-
-write-host -ForegroundColor red "
-                           __  __   ___   ___    ___   ___        
-                          |  \/  | | __| | _ \  / __| | __|       
-                          | |\/| | | _|  |   / | (_ | | _|        
-                          |_|  |_| |___| |_|_\  \___| |___|       
-                          
-                               _____   _  _   ___
-                              |_   _| | || | | __|                
-                                | |   | __ | | _|                 
-                                |_|   |_||_| |___| 
-                        
-                       ___    ___   ___   ___   __    _____   ___
-                      / __|  / __| | _ \ |_ _| | _ \ |_   _| / __|
-                      \__ \ | (__  |   /  | |  |  _/   | |   \__ \
-                      |___/  \___| |_|_\ |___| |_|     |_|   |___/
-                                                                  
-"
-
-Start-Process "https://aelto.github.io/tw3-random-encounters-reworked/indepth-guide/#part-5"
-
-# echo ""
-# write-host -ForegroundColor yellow "Please use the script merger to merge the scripts now."
-# echo "A few exceptions that should not be merged are:"
-# echo " - Conflict between Bootstrap and W3EE, if it happens leave it unmerged, bootstrap will load before."
-# echo " - Conflict between CustomBossBar and W3EE, the installer will install the patch for you and so you are not supposed to merge it."
-# echo " - Conflict between FriendlyHUD and the FHUD sharedutils patch, leave it unmerged."
-# echo ""
-
-# if (test-path ./mods/modW3EE) {
-#   write-host -ForegroundColor yellow "Close the W3EE Merging Instructions image to continue..."
-
-#   Add-Type -AssemblyName 'System.Windows.Forms'
-#   $imageurl = "https://raw.githubusercontent.com/Aelto/tw3-random-encounters-reworked/master/docs/merging-instructions-ee.png"
-#   $imagepath = "./mods/modRandomEncountersReworked/merging-instructions-ee.png"
-
-#   Invoke-WebRequest -Uri $imageurl -OutFile $imagepath
-#   $file = (get-item $imagepath)
-#   $img = [System.Drawing.Image]::Fromfile((get-item $file))
-
-#   [System.Windows.Forms.Application]::EnableVisualStyles()
-#   $form = new-object Windows.Forms.Form
-#   $form.Text = "Random Encounters Reworked | W3EE Merging instructions"
-#   $form.Width = $img.Size.Width / 1.3;
-#   $form.Height =  $img.Size.Height / 1.3;
-#   $pictureBox = new-object Windows.Forms.PictureBox
-#   $pictureBox.Width =  $img.Size.Width / 1.3;
-#   $pictureBox.Height =  $img.Size.Height / 1.3;
-#   $pictureBox.SizeMode = 'Zoom';
-
-#   $pictureBox.Image = $img;
-#   $form.controls.add($pictureBox)
-#   $form.Add_Shown( { $form.Activate() } )
-#   $form.ShowDialog()
-# }
-
-if ((test-path ./mods/modImmersiveCam) -or (test-path ./mods/modW3EE)) {
-  echo ""
-  write-warning "It seems you have Immersive Camera installed."
-  echo "Before loading your save, please go in RER's optional features menu and enable the Immersive Camera compatibility"
-  echo ""
-  echo "Note that if you have Enhanced Edition, EE has a version of Immersive Camera bundled in."
-}
-
-if (test-path ./mods/modBootstrap/content/scripts/game/player/player.ws) {
-  echo ""
-  write-warning "It seems you have Bootstrap Sripthooked installed."
-  echo "Before loading your save, please go in RER's optional features menu and enable the Scripthooked compatibility"
-}
-
-echo ""
-echo ""
-echo "Note that RER comes with an update-script as well, anytime you want to update the mod,"
-echo "simply go inside mods/modRandomEncounters and right-click, run the update-mod.ps1 script."
-echo ""
-echo "Have fun!"
-echo ""
-
-pause
-
-Start-Process "https://aelto.github.io/tw3-random-encounters-reworked/rer-bible/"
+Start-Process "https://aelto.github.io/tw3-random-encounters-reworked/rer-bible#success-install"
